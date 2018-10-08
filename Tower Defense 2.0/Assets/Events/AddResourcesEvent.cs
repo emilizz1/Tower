e1087e8dbe1eb7e40122fa5a27f24790ee976b4e
Emilijus <slibinas2012@gmail.com>
2018-10-08 19:23:44 +0300

    Added resource gain event

---
+++ Tower Defense 2.0/Assets/Events/AddResourcesEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddResourcesEvent : MonoBehaviour
+{
+    [SerializeField] Sprite[] resourcesSprites; 
+
+	public void Activated()
+    {
+        var resourceHolder = FindObjectOfType<ResourcesManager>();
+        resourceHolder.AddResources(2, resourcesSprites[0], transform);
+        resourceHolder.AddResources(2, resourcesSprites[1], transform);
+        resourceHolder.AddResources(2, resourcesSprites[2], transform);
+    }
+}
