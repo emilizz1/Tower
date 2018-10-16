58a14f4c1b94fcf9295aa2e79faa748ec0c5ffba
Emilijus <slibinas2012@gmail.com>
2018-10-16 14:53:48 +0300

    Ideas for future of the project

--- Tower Defense 2.0/Assets/Note.cs
+++ Tower Defense 2.0/Assets/Note.cs
@@ -10,3 +10,14 @@
 // add branching building paths
 // Idea! make line of buildings still comming up. Player to know how much to pay.
 // card shocase pages
+// more events: legendary cards, permanent items, upgrade a card
+// make negative events
+// think of restrictions how many cards can be removed
+
+// Ideas for permanent Items:
+// 00: After level gain 2/1/1 resources
+// 01: Boost for dmg, rng, spd to bow, pistol, throwing characters
+// 02: after building x amount of buildings temple apears which gives gold or wood
+// 03: swamp apears in level, slowed enemies
+// 04: first enemy of level dies automaticly
+// 05: gain/ remove 2 cards
