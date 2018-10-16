a0dbeb840ce03ac5dc6701a7d6f081f2a2a6b02d
Emilijus <slibinas2012@gmail.com>
2018-10-16 10:01:30 +0300

    fixes

--- Tower Defense 2.0/Assets/Note.cs
+++ Tower Defense 2.0/Assets/Note.cs
@@ -8,3 +8,5 @@
 // add playablity with mouse
 // add persistent Items
 // add branching building paths
+// Idea! make line of buildings still comming up. Player to know how much to pay.
+// card shocase pages
