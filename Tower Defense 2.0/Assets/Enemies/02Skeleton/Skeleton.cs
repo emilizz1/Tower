874fb9eb669bd38033eb4f185ffc53b3d78c46fa
Emilijus <slibinas2012@gmail.com>
2018-12-03 21:35:37 +0200

    Fixed bug when dead skeleton would still collide and sand house unit would grow in size.

--- Tower Defense 2.0/Assets/Enemies/02Skeleton/Skeleton.cs
+++ Tower Defense 2.0/Assets/Enemies/02Skeleton/Skeleton.cs
@@ -9,6 +9,7 @@ namespace Towers.Enemies.Skeleton
         [SerializeField] float layingTime = 1f;
 
         bool once = true;
+        bool dead = false;
 
         string RESSURECTION_TRIGGER = "Ressurect";
         string Death_Trigger = "Death";
@@ -17,16 +18,26 @@ namespace Towers.Enemies.Skeleton
         {
             if (once)
             {
+                dead = true;
                 Animator animator = GetComponent<Animator>();
                 animator.SetTrigger(Death_Trigger);
                 yield return new WaitForSecondsRealtime(layingTime);
                 animator.SetTrigger(RESSURECTION_TRIGGER);
                 SetHealthToMax();
+                dead = false;
                 once = false;
             }
             else
             {
                 Death(); 
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (dead)
+            {
+                Physics.IgnoreCollision(GetComponent<Collider>(), collision.gameObject.GetComponent<Collider>());
             }
         }
     }
