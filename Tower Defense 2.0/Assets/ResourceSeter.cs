394ce9cf1fa23ec7d4071e819f58d873b0a8f303
Emilijus <slibinas2012@gmail.com>
2019-01-13 21:50:50 +0200

    Started building ResourceSeter

---
+++ Tower Defense 2.0/Assets/ResourceSeter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Towers.CardN;
+using Towers.Resources;
+
+public class ResourceSeter : MonoBehaviour
+{
+    [SerializeField] GameObject[] ResourceSlots;
+
+    void Start()
+    {
+        SetupCards();
+    }
+
+    void SetupCards()
+    {
+        var myCards = GatherAllCards();
+        
+    }
+
+    List<Card> GatherAllCards()
+    {
+        List<Card> gatheredCards = new List<Card>();
+        foreach(Card card in FindObjectOfType<Deck>().GetLevelCards().GetAllCards())
+        {
+            gatheredCards.Add(card);
+        }
+        foreach (Card card in FindObjectOfType<CardHolders>().GetAllPlayerCards())
+        {
+            gatheredCards.Add(card);
+        }
+        return gatheredCards;
+    }
+
+    List<Resource> GatherDifferentResources(List<Card> myCards)
+    {
+        List<Resource> differentResources = new List<Resource>();
+        foreach(Card card in myCards)
+        {
+            foreach(Resource resource in card.GetPrefabs().GetBuilding(0).GetBuildingUnitCost())
+            {
+                if (!differentResources.Contains(resource))
+                {
+                    differentResources.Add(resource);
+                }
+            }
+        }
+        return differentResources;
+    }
+}
