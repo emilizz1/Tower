3ecce73230193414e978d8318e5953caaf9a921d
Emilijus <slibinas2012@gmail.com>
2019-01-18 21:22:55 +0200

    Added theme music to the game

---
+++ Tower Defense 2.0/Assets/ThemePlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThemePlayer : MonoBehaviour
+{
+    [SerializeField] AudioClip[] audioClips;
+    [SerializeField] float audioVolume = 0.5f;
+
+    bool playing;
+
+    AudioSource audioSource;
+
+    void Awake()
+    {
+        if(FindObjectsOfType<ThemePlayer>().Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    
+    void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = audioVolume;
+	}
+	
+	IEnumerator PlayAudio()
+    {
+        while (playing)
+        {
+            audioSource.clip = audioClips[Random.Range(0, audioClips.Length)];
+            yield return new WaitForSeconds(audioSource.clip.length);
+        }
+    }
+}
