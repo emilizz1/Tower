34a018cc2333e66c7e1f8810f3773b26dd3c56f0
Emilijus <slibinas2012@gmail.com>
2019-02-01 21:47:48 +0200

    Added loading tips and other small changes

---
+++ Tower Defense 2.0/Assets/LoadingTips.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingTips : MonoBehaviour
+{
+    [SerializeField] Sprite[] loadingTips;
+    
+	void Start ()
+    {
+        GetComponent<Image>().sprite = loadingTips[Random.Range(0, loadingTips.Length)];
+	}
+}
