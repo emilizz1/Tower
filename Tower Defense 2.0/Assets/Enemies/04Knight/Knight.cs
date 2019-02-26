da4df49c086d7532699a17630ddb29d3292081e9
Emilijus <slibinas2012@gmail.com>
2019-02-26 20:16:01 +0200

    Changes to level 4 and 6

--- Tower Defense 2.0/Assets/Enemies/04Knight/Knight.cs
+++ Tower Defense 2.0/Assets/Enemies/04Knight/Knight.cs
@@ -9,6 +9,7 @@ namespace Towers.Enemies.Knight
 
         public override void TakeDamage(float damage, Shooter shooter)
         {
+            print("shooter: " + shooter);
             if (shooter.GetAttackType() == Shooter.AttackType.Arrows)
             {
                 damage = damage / 2f;
