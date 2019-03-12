f16ab7d1cfe9f27ed8874ed5722d89f0d7f9ec9d
Emilijus <slibinas2012@gmail.com>
2019-03-12 15:05:09 +0200

    Added menu background with changing colors

---
+++ Tower Defense 2.0/Assets/MainMenuBackground.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuBackground : MonoBehaviour
+{
+    [SerializeField] Color[] bgColors;
+    [SerializeField] float transitionSpeed = 0.1f;
+
+    RawImage image;
+
+    void Start()
+    {
+        image = GetComponent<RawImage>();
+        StartCoroutine(ChangeBgColors());
+    }
+
+    IEnumerator ChangeBgColors()
+    {
+        Color changingTo = bgColors[Random.Range(0, bgColors.Length)];
+        while (true)
+        {
+            image.color = Color.Lerp(image.color, changingTo, transitionSpeed);
+            yield return new WaitForEndOfFrame();
+            if(image.color == changingTo)
+            {
+                changingTo = bgColors[Random.Range(0, bgColors.Length)];
+            }
+            print("changing");
+        }
+    }
+}
