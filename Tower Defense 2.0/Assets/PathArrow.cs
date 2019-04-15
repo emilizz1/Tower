439b90c8ca10f7952111602cfa2d5bca7ef1a376
Emilijus <slibinas2012@gmail.com>
2019-04-15 10:36:09 +0300

    More work on arrow

--- Tower Defense 2.0/Assets/PathArrow.cs
+++ Tower Defense 2.0/Assets/PathArrow.cs
@@ -5,31 +5,51 @@ using Towers.Enemies;
 
 public class PathArrow : MonoBehaviour
 {
+    [SerializeField] float startTurningDistance;
+    [SerializeField] float movementSpeed;
+    [SerializeField] float rotationSpeed;
+
     WaypointContainer waypointContainer;
+    int nextWaypointIndex;
+    Vector3 destination;
 
     void Start()
     {
         waypointContainer = FindObjectOfType<WaypointContainer>();
+        ResetArrow();
     }
-    
+
     void Update()
     {
-        
+        CycleWaypoint();
+        if (destination != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination, movementSpeed);
+            RotateArrow();
+        }
     }
 
     void CycleWaypoint()
     {
-
+        if (waypointContainer != null)
+        {
+            destination = waypointContainer.transform.GetChild(nextWaypointIndex).position;
+            if (Vector3.Distance(transform.position, destination) <= startTurningDistance)
+            {
+                nextWaypointIndex = (nextWaypointIndex + 1) % waypointContainer.transform.childCount;
+                if (nextWaypointIndex == 0)
+                {
+                    ResetArrow();
+                }
+            }
+        }
     }
 
-    void Move()
+    void RotateArrow()
     {
-
-    }
-
-    void Turn()
-    {
-
+        var _lookRotation = Quaternion.LookRotation((destination - transform.position).normalized);
+        print(Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * rotationSpeed));
+        transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, rotationSpeed);
     }
 
     public void ArrowActive()
@@ -44,6 +64,7 @@ public class PathArrow : MonoBehaviour
 
     void ResetArrow()
     {
-
+        nextWaypointIndex = 0;
+        transform.position = waypointContainer.transform.GetChild(nextWaypointIndex).position;
     }
 }
