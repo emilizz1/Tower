d5c520aad4300c894019d6039aff55e2ed45d600
Emilijus <slibinas2012@gmail.com>
2019-04-22 15:48:12 +0300

    Started working on new unit

---
+++ Tower Defense 2.0/Assets/Enemies/07BlackMarketer/BlackMarketer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Towers.Resources;
+
+namespace Towers.Enemies.BlackMarketer
+{
+
+    public class BlackMarketer : HealthSystem
+    {
+        [SerializeField] string special;
+
+        public override IEnumerator KillCharacter()
+        {
+            GiveGold();
+            return base.KillCharacter();
+        }
+
+        void GiveGold()
+        {
+            Resource[] goldResources = new Resource[1];
+            goldResources[0] = new ResourceGold();
+            FindObjectOfType<ResourcesManager>().AddResources(goldResources);
+        }
+
+        public override string GetSpecial()
+        {
+            return special;
+        }
+    }
+}
