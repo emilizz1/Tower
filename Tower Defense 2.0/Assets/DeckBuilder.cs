46712a246821c6b96250b6f7c156af65199be264
Emilijus <slibinas2012@gmail.com>
2019-04-27 22:41:02 +0300

    Finished writing deck builder

--- Tower Defense 2.0/Assets/DeckBuilder.cs
+++ Tower Defense 2.0/Assets/DeckBuilder.cs
@@ -1,29 +1,73 @@
-﻿using System;
-using System.Collections;
+﻿using UnityEngine;
+using Towers.CardN;
 using System.Collections.Generic;
-using UnityEngine;
+using Towers.BuildingsN;
 
-public class DeckBuilder : MonoBehaviour
+namespace Towers.Scenes.RunSelection
 {
-    // Start is called before the first frame update
-    void Start()
+    public class DeckBuilder : MonoBehaviour
     {
-        
-    }
+        [SerializeField] GameObject leftChoice, rightChoice;
+        [SerializeField] CardHolder addableCards;
+        [SerializeField] int cardsToDraft = 6; //make private
+ 
+        CardHolder cardsDrafted;
+        Card leftCard, rightCard;
+        List<Buildings> buildings = new List<Buildings>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        void Start()
+        {
+            cardsDrafted = new CardHolder();
+            PrepareNewCards();
+        }
 
-    internal void CardChosen(int choice)
-    {
-        throw new NotImplementedException();
-    }
+        public void CardChosen(int choice)
+        {
+            if(choice == 0)
+            {
+                cardsDrafted.AddCard(leftCard);
+                buildings.Add(leftCard.GetPrefabs().GetBuilding(0));
+                addableCards.AddCard(rightCard);
+            }
+            else
+            {
+                cardsDrafted.AddCard(rightCard);
+                buildings.Add(rightCard.GetPrefabs().GetBuilding(0));
+                addableCards.AddCard(leftCard);
+            }
+            if (!IsFinished())
+            {
+                PrepareNewCards();
+            }
+        }
 
-    internal bool IsFinished()
-    {
-        throw new NotImplementedException();
+        public bool IsFinished()
+        {
+            return cardsDrafted.GetAllCards().Length == cardsToDraft;
+        }
+
+        void PrepareNewCards()
+        {
+            leftCard = addableCards.GetAllCards()[Random.Range(0, addableCards.GetAllCards().Length)];
+            leftChoice.GetComponentInChildren<ShowcaseCard>().PutInformation(leftCard, GetBuildingLevel(leftCard));
+            addableCards.RemoveCard(leftCard);
+            rightCard = addableCards.GetAllCards()[Random.Range(0, addableCards.GetAllCards().Length)];
+            rightChoice.GetComponentInChildren<ShowcaseCard>().PutInformation(rightCard, GetBuildingLevel(rightCard));
+            addableCards.RemoveCard(rightCard);
+        }
+
+        int GetBuildingLevel(Card card)
+        {
+            int buildingLevel = 0;
+            Buildings currentlyLooking = card.GetPrefabs().GetBuilding(0);
+            foreach (Buildings building in buildings)
+            {
+                if (building == currentlyLooking)
+                {
+                    buildingLevel++;
+                }
+            }
+            return buildingLevel;
+        }
     }
 }
