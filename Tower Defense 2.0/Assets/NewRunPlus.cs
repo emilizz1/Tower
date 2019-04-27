d3fe110c63c70a03277c14e1566eeb2a5eaae8c2
Emilijus <slibinas2012@gmail.com>
2019-04-27 10:00:59 +0300

    More work on new game plus

---
+++ Tower Defense 2.0/Assets/NewRunPlus.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewRunPlus : MonoBehaviour
+{
+    bool shouldCardDraft = false;
+
+    public void SetCardDraft(bool value)
+    {
+        shouldCardDraft = value;
+    }
+
+    public bool GetCardDraft()
+    {
+        return shouldCardDraft;
+    }
+}
